25a79e3a49772fd99787382b8d0a436765c459e9
Felipe Farnetti <Felipefarnetti@gmail.com>
2023-04-27 16:51:10 +0200

    first commit

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,79 +1,81 @@
 import { useState } from "react";
 
 const Form = () => {
-  // inputs du form //
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
-
-  // message d'erreur si pas tous les champs remplis//
   const [errorMessage, setErrorMessage] = useState("");
+
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
 
-  const handlePasswordChange1 = (event) => {
+  const handlePasswordConfirmChange = (event) => {
     setPasswordConfirm(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (
-      (password === passwordConfirm && name === useState) ||
-      email === useState ||
-      password === useState
-    ) {
-      setErrorMessage("");
-      alert("Votre compte a été créé");
-    } else {
-      setErrorMessage("Le mot de passe n'est pas idéntique");
+    if (!name || !email || !password || !passwordConfirm) {
+      setErrorMessage("Veuillez remplir tous les champs");
+      return;
     }
+    if (password !== passwordConfirm) {
+      setErrorMessage("Les mot de passe ne sont pas identiques");
+      return;
+    }
+    setErrorMessage("");
+    alert("Le compte a été créé");
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <span>Name</span>
       <input
-        placeholder="Jean Dupont"
         type="text"
+        id="name"
         name="name"
-        onChange={(event) => {
-          setName(event.target.value);
-        }}
         value={name}
+        onChange={(event) => setName(event.target.value)}
       />
       <span>Email</span>
+
       <input
-        placeholder="Email"
         type="email"
+        id="email"
         name="email"
-        onChange={(event) => {
-          setEmail(event.target.value);
-        }}
         value={email}
+        onChange={(event) => setEmail(event.target.value)}
       />
       <span>Password</span>
       <input
-        placeholder="password"
         type="password"
+        id="password"
         name="password"
-        onChange={handlePasswordChange}
         value={password}
+        onChange={handlePasswordChange}
       />
-      <span>Confirm your password</span>
+      <span>Corfirm your password</span>
       <input
-        placeholder="confirm password"
         type="password"
+        id="confirmPassword"
         name="confirmPassword"
-        onChange={handlePasswordChange1}
         value={passwordConfirm}
+        onChange={handlePasswordConfirmChange}
       />
-      <button type="submit" value="Register">
-        Register
-      </button>
+      <button type="submit">Register</button>
       {errorMessage && (
-        <p style={{ color: "red", fontSize: "12px" }}>{errorMessage}</p>
+        <p
+          style={{
+            color: "red",
+            fontSize: "12px",
+            margin: "10px",
+            textAlign: "center",
+          }}
+        >
+          {errorMessage}
+        </p>
       )}
     </form>
   );
