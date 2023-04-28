2f7debd28ca296b9f984731306da5b8b3e31df4b
Felipe Farnetti <Felipefarnetti@gmail.com>
2023-04-28 10:08:45 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ import StepTwo from "./Components/StepTwo";
 import Footer from "./Components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="container">
       <h1>Create account</h1>
